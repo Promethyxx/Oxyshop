3aa606ea8130cbe0021a5220881eb60f2d1cabf7
Promethyxx <peter.mboyo@ik.me>
2026-04-04 14:17:51 +0200

    kinoite fix

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 fn main() {
     slint_build::compile("ui/main.slint").unwrap();
 
-    let target = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
-    if target == "windows" {
+    #[cfg(target_os = "windows")]
+    {
         let icon = std::path::Path::new("assets/Oxyshop_icon.ico");
         if icon.exists() {
             let mut res = winresource::WindowsResource::new();
