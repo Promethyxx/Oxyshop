76de6b507da5869a1335fa4b4e071cfa4deddbab
Promethyxx <peter.mboyo@ik.me>
2026-04-25 17:39:28 +0200

    build.rs fix android

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 fn main() {
     slint_build::compile("ui/main.slint").unwrap();
 
-    #[cfg(target_os = "windows")]
-    {
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    if target_os == "windows" {
         let icon = std::path::Path::new("assets/Oxyshop_icon.ico");
         if icon.exists() {
             let mut res = winresource::WindowsResource::new();
