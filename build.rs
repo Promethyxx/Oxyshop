33fd7ecc736ce1e3a9e43f6491ca1b6a8cb678a2
Promethyxx <peter.mboyo@ik.me>
2026-04-27 02:11:49 +0200

    build.rs fix linux

--- build.rs
+++ build.rs
@@ -1,13 +1,17 @@
 fn main() {
     slint_build::compile("ui/main.slint").unwrap();
+    windows_resources();
+}
 
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
-    if target_os == "windows" {
-        let icon = std::path::Path::new("assets/Oxyshop_icon.ico");
-        if icon.exists() {
-            let mut res = winresource::WindowsResource::new();
-            res.set_icon(icon.to_str().unwrap());
-            res.compile().expect("Failed to compile Windows resources");
-        }
+#[cfg(target_os = "windows")]
+fn windows_resources() {
+    let icon = std::path::Path::new("assets/Oxyshop_icon.ico");
+    if icon.exists() {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon(icon.to_str().unwrap());
+        res.compile().expect("Failed to compile Windows resources");
     }
 }
+
+#[cfg(not(target_os = "windows"))]
+fn windows_resources() {}
